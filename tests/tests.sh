a25f2c46d69fe0918da67b31b6f79ef32c3ad69d
Tadeusz Struk <tstruk@gmail.com>
2022-01-22 17:51:56 -0800

    Cleanup test script
    Signed-off-by: Tadeusz Struk <tadeusz.struk@linaro.org>

--- tests/tests.sh
+++ tests/tests.sh
@@ -9,16 +9,17 @@ echo "RUNNING TESTS"
 ########################
 # Test generating key
 ########################
-foreach KEY ( $KEYS )
+foreach KEY ($KEYS)
 echo "######### ${KEY} #################"
+	echo ./${PROG} -g -c ${KEY} -okeys/${KEY}.pem
 	./${PROG} -g -c ${KEY} -okeys/${KEY}.pem
 	ls -l keys/${KEY}.pem
 
-	if( $? == 0 ) then
+	if($? == 0) then
 		echo ${KEY} generated ok
 	else
 		echo Gen key ${KEY} failed
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
+		echo "Test Failed!"
 		exit
 	endif
 end
@@ -30,14 +31,15 @@ echo "##################################"
 echo "Exporting public Keys"
 echo "##################################"
 
-foreach KEY ( $KEYS )
+foreach KEY ($KEYS)
 echo "######### ${KEY} #################"
+	echo ./${PROG} -x -kkeys/${KEY}.pem -okeys/public_${KEY}.pem
 	./${PROG} -x -kkeys/${KEY}.pem -okeys/public_${KEY}.pem
-	if( $? == 0 ) then
+	if($? == 0) then
 		echo ${KEY} key exported ok
 	else
 		echo Key export ${KEY} failed
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
+		echo "Test Failed!"
 		exit
 	endif
 end
@@ -45,32 +47,35 @@ end
 ########################
 # Test singing message
 ########################
-foreach KEY ( $KEYS )
+foreach KEY ($KEYS)
 	echo "######### ${KEY} signing the message  #################"
+	echo ./${PROG} -s -kkeys/${KEY}.pem -omessage.txt.sign message.txt
 	./${PROG} -s -kkeys/${KEY}.pem -omessage.txt.sign message.txt
-	if( $? == 0 ) then
+	if($? == 0) then
 		echo Message signed ok
 	else
 		echo Message signed failed
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
+		echo "Test Failed!"
 		exit
 	endif
 
 	echo "######### ${KEY} verifying the message  #################"
+	echo ./${PROG} -v -kkeys/public_${KEY}.pem -imessage.txt.sign message.txt
 	./${PROG} -v -kkeys/public_${KEY}.pem -imessage.txt.sign message.txt
-	if( $? == 0 ) then
+	if($? == 0) then
 		echo Message signed ok
 	else
 		echo Message signed failed
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
+		echo "Test Failed!"
 		exit
 	endif
 
 		echo "######### ${KEY} verifying the message_changed  #################"
+	echo ./${PROG} -v -kkeys/public_${KEY}.pem -imessage.txt.sign message_changed.txt
 	./${PROG} -v -kkeys/public_${KEY}.pem -imessage.txt.sign message_changed.txt
-	if( $? == 0 ) then
+	if($? == 0) then
 		echo Message signature ok - test failed
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
+		echo "Test Failed!"
 		exit
 	else
 		echo Singature is not valid - test ok
@@ -80,55 +85,51 @@ end
 ########################
 # Test encrypt/decrypt
 ########################
-foreach KEY ( $KEYS )
+foreach KEY ($KEYS)
 	echo "######### ${KEY} encrypting data  #################"
+	echo ./${PROG} -e -kkeys/public_${KEY}.pem message.txt
 	./${PROG} -e -kkeys/public_${KEY}.pem message.txt
-	if( $? == 0 ) then
+	if($? == 0) then
 		echo Message encrypted ok
 	else
 		echo Message encrypt failed
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
+		echo "Test Failed!"
 		exit
 	endif
 
 	echo "######### ${KEY} decrypting data  #################"
+	echo ./${PROG} -d -kkeys/${KEY}.pem -o message.txt.dec message.txt.enc
 	./${PROG} -d -kkeys/${KEY}.pem -o message.txt.dec message.txt.enc
-	if( $? == 0 ) then
+	if($? == 0) then
 		echo Message decrypted ok
 	else
 		echo Message decrypt failed
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
+		echo "Test Failed!"
 		exit
 	endif
 
 	diff message_orign.txt message.txt.dec
-	if( $? != 0 ) then
+	if($? != 0) then
 		echo Message decrypted file is different
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
+		echo "Test Failed!"
 		exit
 	endif
 
+	echo ./${PROG} -d -kkeys/${KEY}.pem -omessage.txt.decrypted message.txt.enc
 	./${PROG} -d -kkeys/${KEY}.pem -omessage.txt.decrypted message.txt.enc
-	if( $? == 0 ) then
+	if($? == 0) then
 		echo Message decrypted ok
 	else
 		echo Message dncrypt failed
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
+		echo "Test Failed!"
 		exit
 	endif
 
 	diff message_orign.txt message.txt.decrypted
-	if( $? != 0 ) then
+	if($? != 0) then
 		echo Message decrypted file is different
-		echo "!!!!!!!!!!!!!! FAILED !!!!!!!!!!!!!!!"
-		exit
+		echo "Test Failed!"
+		exit -1
 	endif
 end
-
 echo "ALL TESTS PASSED"
-
-
-
-
-
-
